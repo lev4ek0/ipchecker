a2c2357e073270ebe687de41c1c94e5d95757a1e
Lev Belous <leva22.08.01@inbox.ru>
2021-08-19 23:17:05 +0300

    w

--- routes.py
+++ routes.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, request, json, render_template, url_for, redirect, flash, session
+import requests
 
 route_gate = Blueprint('route_gate', __name__)
 
 
 @route_gate.route('/', methods=['GET'])
 def index():
-    print(request.environ['REMOTE_ADDR'])
+    ip = request.environ['REMOTE_ADDR']
+    url = 'http://freegeoip.net/json/' + ip
+    r = requests.get(url)
+    js = r.json()
+    print(js['country_code'])
     return 'hello'
