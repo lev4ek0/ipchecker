d45dd812cca5dc20bfcfdb473e30d797996be20a
Lev Belous <leva22.08.01@inbox.ru>
2021-08-19 23:21:52 +0300

    w

--- routes.py
+++ routes.py
@@ -4,11 +4,20 @@ import requests
 route_gate = Blueprint('route_gate', __name__)
 
 
+def getting_ip(row):
+    """This function calls the api and return the response"""
+    url = f"https://freegeoip.app/json/{row}"       # getting records from getting ip address
+    headers = {
+        'accept': "application/json",
+        'content-type': "application/json"
+        }
+    response = requests.request("GET", url, headers=headers)
+    respond = json.loads(response.text)
+    return respond
+
+
 @route_gate.route('/', methods=['GET'])
 def index():
     ip = request.environ['REMOTE_ADDR']
-    url = 'http://freegeoip.net/json/' + ip
-    r = requests.get(url)
-    js = r.json()
-    print(js['country_code'])
+    print('Страна:', getting_ip(ip).get('country_name'))
     return 'hello'
